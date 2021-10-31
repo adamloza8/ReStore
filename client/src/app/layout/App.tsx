bc1908319e9c6d1d6630aff2217bfb65074548f4
adamloza8 <adamloza8@gmail.com>
2021-10-31 21:03:04 +0100

    Stage 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,12 +1,16 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/catalog/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
 import ContactPage from "../../features/catalog/contact/ContactPage";
 import HomePage from "../../features/catalog/home/HomePage";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false)
@@ -25,17 +29,24 @@ function App() {
     setDarkMode(!darkMode);
   }
 
-  return (
+return (
     <>
+    
+
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar/>
      <CssBaseline />
      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
      <Container>
+     <Switch>
      <Route exact path='/' component={HomePage} />  
      <Route exact path='/catalog' component={Catalog} />  
      <Route path='/catalog/:id' component={ProductDetails} />  
      <Route path='/about' component={AboutPage} />  
      <Route path='/contact' component={ContactPage} />  
+     <Route path='/server-error' component={ServerError} />  
+     <Route component={NotFound} />  
+     </Switch>
      </Container>
      </ThemeProvider>
      </>
